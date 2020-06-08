0fe809eb1d6cf050dbbdb44a0f51039d70ff97fe
somyaj15 <somyaj152001@gmail.com>
2020-06-08 13:42:54 +0530

    end page

---
+++ hello/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      <div class="container">
+   
+  <h2>THANK YOU !!</h2>
+   <div class="panel panel-default">
+     <div class="panel-body"><b>Your shipment is confirmed!!</b>
+     <div>We have accepted your order, and are getting it ready.<div>Come back to this page for updates on your shipment status. </div></div></div>
+   </div>
+ </div>
+ 
+ <img class="image mt-4" src="https://media3.giphy.com/media/kigfYxdEa5s1ziA2h1/source.gif"></img>
+    
+      </header>
+    </div>
+  );
+}
+
+export default App;
